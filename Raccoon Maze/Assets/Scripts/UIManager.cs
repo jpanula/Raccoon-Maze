3ce562050fb1f4953fb6ddffe9c417a6a8a16b44
Jaakko Panula <panula.jaakko@gmail.com>
2018-09-26 10:43:02 +0300

    keskeneräinen UIManager

---
+++ Raccoon Maze/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+	[SerializeField]
+	private bool[] _activePlayers = new bool[4];
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < _activePlayers.Length; i++)
+		{
+			string playerName = "Player" + (i + 1);
+			_activePlayers[i] = GameObject.Find(playerName);
+		}
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
