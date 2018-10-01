6cf9a7a572d2c0e8368b8f5aec23f0e08e6fd6bd
Jaakko Panula <panula.jaakko@gmail.com>
2018-10-01 15:35:15 +0300

    added hp bar prefab for players

---
+++ Raccoon Maze/Assets/Scripts/HPBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+
+	private int _initialHP;
+	private GameObject parent;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		parent = this.transform.parent.gameObject;
+		_initialHP = GetParentHP();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.GetChild(0).transform.localScale = new Vector3((float) GetParentHP() / _initialHP, 1, 1);
+	}
+
+	private void LateUpdate()
+	{
+		transform.rotation = Quaternion.identity;
+	}
+
+	private int GetParentHP()
+	{
+		if (parent.GetComponent<Player>())
+		{
+			return parent.GetComponent<Player>().HP;
+		}
+
+		return 0;
+	}
+}
