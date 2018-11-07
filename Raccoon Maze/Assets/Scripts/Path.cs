aa1c20b15bc3fe1b0ab5ddbf53e759cdac9d17dd
Jaakko <panula.jaakko@gmail.com>
2018-11-07 12:34:40 +0200

    Added a script for the path parent of the path nodes
    it draws lines between the nodes and provides easy access to the nodes it has

---
+++ Raccoon Maze/Assets/Scripts/Path.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+
+    public PathNode[] Nodes
+    {
+        get { return GetNodes(); }
+    }
+
+    private PathNode[] GetNodes()
+    {
+        return GetComponentsInChildren<PathNode>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        PathNode[] nodes = GetNodes();
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            Gizmos.DrawLine(nodes[i].GetPosition(), nodes[(i + 1) % nodes.Length].GetPosition());
+        }
+    }
+}
