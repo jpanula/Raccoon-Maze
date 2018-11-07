8a534ac0c90d21610cbdf0d61c8d36c9159c9f47
Jaakko <panula.jaakko@gmail.com>
2018-11-07 13:00:10 +0200

    WIP started work on path follower

---
+++ Raccoon Maze/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField] private Path _path;
+    private PathNode[] _nodes;
+    private float _moveSpeed;
+    private int _currentNode;
+
+    private void Start()
+    {
+        _nodes = _path.GetNodes();
+        _currentNode = 0;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private PathNode GetClosestNode()
+    {
+        PathNode closestNode = _nodes[0];
+        float distance = Vector3.Distance(transform.position, _nodes[0].GetPosition());
+        float newDistance = 0;
+        foreach (var node in _nodes)
+        {
+            newDistance = Vector3.Distance(transform.position, node.GetPosition());
+            
+            if (newDistance < distance)
+            {
+                closestNode = node;
+                distance = newDistance;
+            }
+        }
+
+        return closestNode;
+    }
+
+    private void MoveTowardsNextNode()
+    {
+        
+    }
+}
