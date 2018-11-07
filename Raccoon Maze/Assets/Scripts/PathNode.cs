1f722a2eee6a886a0ff9abcbb8ebba7c36a1d7d5
Jaakko <panula.jaakko@gmail.com>
2018-11-07 11:49:40 +0200

    Added scripts/classes for path nodes

---
+++ Raccoon Maze/Assets/Scripts/PathNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PathNode : MonoBehaviour
+{
+    public enum NodeType
+    {
+        Curved,
+        Straight
+    }
+
+    public abstract NodeType Type { get; }
+
+    public virtual Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+}
