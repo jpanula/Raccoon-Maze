3cde4a1ac10be6eb23cdf72e376d5fc8d4d523aa
Jaakko <panula.jaakko@gmail.com>
2018-11-09 12:47:24 +0200

    Made a script for a simple spinning object

---
+++ Raccoon Maze/Assets/Scripts/Spinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField, Tooltip("Negative values spin to the other direction")] private float _speed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward, _speed * Time.deltaTime);
+    }
+}
